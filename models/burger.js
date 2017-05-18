df60a107ecd2018bdfc4ed02fe4a1a9474af429c
Phil Sautter <psautter@gmail.com>
2017-05-18 15:32:35 -0400

    created burger model

--- models/burger.js
+++ models/burger.js
@@ -1,20 +1,25 @@
 const orm = require('../config/orm.js');
 
 module.export = function all(callback) {
+  // callback : function
   orm.selectAll('burgers', (result) => {
     callback(result);
   });
 };
 
 module.export = function add(name, callback) {
+  // name : string
+  // callback : function
   orm.insertOne('burgers', 'burger_name', name, (result) => {
     callback(result);
   });
 };
 
-module.export = function devour(id, callback) {
+module.export = function devour(burgerId, callback) {
+  // burgerId : integer
+  // callback : function
   orm.updateOne('burgers', 'devoured', {
-    'id': id
+    id: burgerId,
   }, (result) => {
     callback(result);
   });
