085e43858ec4daa1292ec5512a79127efaeb4d32
Phil Sautter <psautter@gmail.com>
2017-05-19 07:31:24 -0400

    fixed issue where devoured was passed as a str when it should be an obj

--- models/burger.js
+++ models/burger.js
@@ -18,7 +18,9 @@ exports.add = (name, callback) => {
 exports.devour = (burgerId, callback) => {
   // burgerId : integer
   // callback : function
-  orm.updateOne('burgers', 'devoured', {
+  orm.updateOne('burgers', {
+    devoured: 1
+  }, {
     id: burgerId,
   }, (result) => {
     callback(result);
