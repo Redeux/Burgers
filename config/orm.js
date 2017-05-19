b92015431bc928de8d8f84df267276131677aefd
Phil Sautter <psautter@gmail.com>
2017-05-19 14:32:02 -0400

    changed to database connection pooling

--- config/orm.js
+++ config/orm.js
@@ -1,60 +1,71 @@
 const dbConfig = process.env.CLEARDB_DATABASE_URL || require('./connection');
 const mysql = require('mysql');
 
+// Perform query against all items in burgers table
 exports.selectAll = (table, callback) => {
   // table : string
 
-  // Create a new connection to the database
-  const connection = mysql.createConnection(dbConfig);
+  // Create a new connection pool for the database
+  const pool = mysql.createPool(dbConfig);
   // Connect to the database
-  connection.connect();
-  // Perform query against all items in burgers table
-  connection.query('SELECT * FROM ??', table, (err, rows) => {
-    // When a response is received disconnect from the database
-    connection.end();
-    // If the response is an error return it
-    // if (process.env.node_env === 'development') if (err) throw err;
-    // on valid data return it
-    return callback(rows);
+  pool.getConnection((err, connection) => {
+    if (connection) {
+      connection.query('SELECT * FROM ??', table, (err, rows) => {
+        // Release the connection on response
+        connection.release();
+        // If the response is an error return it
+        // if (process.env.node_env === 'development') if (err) throw err;
+        // on valid data return it
+        return callback(rows);
+      });
+    }
   });
 };
 
+// Insert one new row into a table
 exports.insertOne = (table, column, value, callback) => {
   // table : string
   // column : string
   // value : string
 
-  // Create a new connection to the database
-  const connection = mysql.createConnection(dbConfig);
+  // Create a new connection pool for the database
+  const pool = mysql.createPool(dbConfig);
   // Connect to the database
-  connection.connect();
-  // Insert a new row into a table
-  connection.query('INSERT INTO ?? (??) VALUES (?)', [table, column, value], (err, row) => {
-    // When a response is received disconnect from the database
-    connection.end();
-    // If the response is an return return it
-    // if (process.env.node_env === 'development') if (err) throw err;
-    // on valid data return it
-    return callback(row);
+  pool.getConnection((err, connection) => {
+    if (connection) {
+      // Insert a new row into a table
+      connection.query('INSERT INTO ?? (??) VALUES (?)', [table, column, value], (err, row) => {
+        // Release the connection on response
+        connection.release();
+        // If the response is an return return it
+        // if (process.env.node_env === 'development') if (err) throw err;
+        // on valid data return it
+        return callback(row);
+      });
+    }
   });
 };
 
+// Update the value of one column in one row of a table
 exports.updateOne = (table, column, where, callback) => {
   // table : string
   // column : object
   // where : object
 
-  // Create a new connection to the database
-  const connection = mysql.createConnection(dbConfig);
+  // Create a new connection pool for the database
+  const pool = mysql.createPool(dbConfig);
   // Connect to the database
-  connection.connect();
-  // Update the burger to devoured based on the id
-  connection.query('UPDATE ?? SET ? WHERE ?', [table, column, where], (err, row) => {
-    // When a response is received disconnect from the database
-    connection.end();
-    // If the response is an error return it
-    // if (process.env.node_env === 'development') if (err) throw err;
-    // on valid data return it
-    return callback(row);
+  pool.getConnection((err, connection) => {
+    if (connection) {
+      // Update the burger to devoured based on the id
+      connection.query('UPDATE ?? SET ? WHERE ?', [table, column, where], (err, row) => {
+        // Release the connection on response
+        connection.release();
+        // If the response is an error return it
+        // if (process.env.node_env === 'development') if (err) throw err;
+        // on valid data return it
+        return callback(row);
+      });
+    }
   });
 };
